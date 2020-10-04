fd91d3ec7729ff76fccf231ad2c082654af2c7ef
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-04 09:45:40 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -5,24 +5,40 @@ Pythagorean triples are sets of 3 integers such that the squares of the 2 smalle
 Ask the user to enter in 3 numbers, in any order.  Order the numbers from smallest to largest.
 Determine if they form a Pythagorean triple. 
 (2 marks)
-
 Inputs:
 an integer
 an integer
 an integer
-
 Outpus:
 xx,yy,zz form a Pythagorean Triple
 xx,yy,zz do not form a Pythagorean Triple
-
 Examples:
 Enter an integer=>3
 Enter an integer=>5
 Enter an integer=>4
 3,4,5 form a Pythagorean triple
-
 Enter an integer=>5
 Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+number1 = int(input("Enter an interger"))
+number2 = int(input("Enter an interger"))
+number3 = int(input("Enter an interger"))
+maxval = max(number1, number2, number3)
+minval = min(number1, number2, number3)
+midval = (number1 + number2 + number3) - (maxval + minval)
+sqmin = minval ** 2
+sqmid = midval ** 2
+sqmax = maxval ** 2
+firsthalf = sqmin + sqmid
+if firsthalf == sqmax:
+    snumber1 = str(number1)
+    snumber2 = str(number2)
+    snumber3 = str(number3)
+    print(snumber1 + "," + snumber2 + "," + snumber3 + ", form a Pythagorean Triple")
+else:
+    snumber1 = str(number1)
+    snumber2 = str(number2)
+    snumber3 = str(number3)
+    print(snumber1 + "," + snumber2 + "," + snumber3 + " do not form a Pythagorean Triple")
